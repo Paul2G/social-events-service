4c9f71a9e64dde2c817d07e5ff952ce862980059
Paul2G <pggaleana@gmail.com>
2024-11-02 23:43:30 -0700

    feat: create `SocialEvent` controller with get routes

---
+++ wep-api-learning/Controllers/SocialEventsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wep_api_learning.Data;
+
+namespace wep_api_learning.Controllers;
+
+[Route("api/social-events")]
+[ApiController]
+public class SocialEventsController: ControllerBase
+{
+    private readonly ApplicationDBContext _context;
+    
+    public SocialEventsController(ApplicationDBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var socialEvents = _context.SocialEvents.ToList();
+
+        return Ok(socialEvents);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] long id)
+    {
+        var socialEvent = _context.SocialEvents.Find(id);
+
+        if (socialEvent == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(socialEvent);
+    }
+}
