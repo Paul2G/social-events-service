35907184eb1c2f8686bbea0b4965dfa1b03854bf
Paul2G <pggaleana@gmail.com>
2025-06-01 22:10:43 -0700

    refactor: update model builder setup

--- social-events-manager/Data/ApplicationDBContext.cs
+++ social-events-manager/Data/ApplicationDBContext.cs
@@ -8,35 +8,33 @@ using social_events_manager.Modules.SocialEvents.Models;
 
 namespace social_events_manager.Data;
 
-public class ApplicationDbContext(DbContextOptions dbContextOptions) : IdentityDbContext<AppUser>(dbContextOptions)
+public class ApplicationDbContext(DbContextOptions dbContextOptions)
+    : IdentityDbContext<AppUser>(dbContextOptions)
 {
     public DbSet<SocialEvent> SocialEvents { get; set; }
     public DbSet<Attendee> Attendees { get; set; }
     public DbSet<Location> Locations { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(builder);
+        base.OnModelCreating(modelBuilder);
 
-        // Config many-to-many relationship
-        builder.Entity<SocialEvent>()
-            .HasMany(e => e.Attendees)
-            .WithMany(e => e.SocialEvents);
-
-        // Seeding roles
-        var roles = new List<IdentityRole>
-        {
-            new()
-            {
-                Name = "Admin",
-                NormalizedName = "ADMIN"
-            },
-            new()
-            {
-                Name = "User",
-                NormalizedName = "USER"
-            }
-        };
-        builder.Entity<IdentityRole>().HasData(roles);
+        modelBuilder
+            .Entity<Attendee>()
+            .HasMany(a => a.SocialEvents)
+            .WithMany(se => se.Attendees)
+            .UsingEntity<Dictionary<string, object>>(
+                "AttendeeSocialEvent",
+                j =>
+                    j.HasOne<SocialEvent>()
+                        .WithMany()
+                        .HasForeignKey("SocialEventsId")
+                        .OnDelete(DeleteBehavior.Cascade), // or NoAction
+                j =>
+                    j.HasOne<Attendee>()
+                        .WithMany()
+                        .HasForeignKey("AttendeesId")
+                        .OnDelete(DeleteBehavior.Cascade) // choose only one to cascade
+            );
     }
 }
